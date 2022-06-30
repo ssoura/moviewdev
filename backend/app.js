5a475a510ee0aa06d8dbf19cb434d6c5756e52e7
Sourabh Shukla <34943727+ssoura@users.noreply.github.com>
2022-06-30 20:00:07 +0530

    init

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const { errorHandler } = require("./middlewares/error");
+const cors = require("cors");
+
+require("dotenv").config();
+require("./db");
+const userRouter = require("./routes/user");
+const actorRouter = require("./routes/actor");
+const movieRouter = require("./routes/movie");
+const reviewRouter = require("./routes/review");
+const { handleNotFound } = require("./utils/helper");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api/user", userRouter);
+app.use("/api/actor", actorRouter);
+app.use("/api/movie", movieRouter);
+app.use("/api/review", reviewRouter);
+
+app.use("/*", handleNotFound);
+
+app.use(errorHandler);
+
+// app.post(
+//   "/sign-in",
+//   (req, res, next) => {
+//     const { email, password } = req.body;
+//     if (!email || !password)
+//       return res.json({ error: "email/password missing!" });
+//     next();
+//   },
+//   (req, res) => {
+//     res.send("<h1>Hello I am about page</h1>");
+//   }
+// );
+
+app.listen(8000, () => {
+  console.log("the port is listening on port 8000");
+});
